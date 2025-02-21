7bc88e1d987f7ad8a638251bf011b1b43dc9b893
caioinnocencio@gmail.com <caioinnocencio@gmail.comgit config user.email caioinnocencio@gmail.com>
2025-02-20 21:59:49 -0300

    Modificando a Tela.tsx

--- efeitos-ufjf/src/Tela.tsx
+++ efeitos-ufjf/src/Tela.tsx
@@ -1,24 +1,26 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react"
 
 export default function Tela(){
-    const [posicao, setPosicao] = useState({x: 150, y:75});
+    const refCanvas = useRef<HTMLCanvasElement | null>(null);
+    const [posicao, setPosicao] = useState({x:150,y:75});
+    
+    useEffect(()=>{
+            const canvas = refCanvas.current;
+            if(!canvas) return;
+            const ctx = canvas.getContext("2d");
+            if(!ctx) return;
+            ctx.fillStyle = "lightblue";
+            ctx.fillRect(0,0,canvas.width, canvas.height);
+            ctx.fillStyle = "lightgreen";
+            ctx.ellipse(posicao.x,posicao.y,5,5,0,0,2*Math.PI,false);
+            ctx.stroke();
+            ctx.fill();
+    });
 
-const refCanvas = useRef<HTMLCanvasElement|null>(null);
+    
 
-return <canvas ref={refCanvas} onClick = {() => {
-    const canvas = refCanvas.current;
-    if(!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if(!ctx) return;
-    ctx.fillStyle = "lightblue"
-    ctx.fillRect(0,0,canvas.width, canvas.height);
-    ctx.fillStyle = "lightgreen";
-    ctx.ellipse(posicao.x, posicao.y, 5, 5, 0, 0, 2*Math.PI, false);
-    ctx.stroke();
-    ctx.fill();
-}}
-
-
-/>
+    return <canvas ref={refCanvas} onClick={()=>{
+        setPosicao({...posicao, x: posicao.x + 5})
+    }}></canvas>
 
 }
